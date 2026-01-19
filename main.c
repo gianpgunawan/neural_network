c1e319793e6ddb4157cbc39b5e390c9c54674f06
Gian PG <gian.pradipta@ti.ukdw.ac.id>
2026-01-19 20:38:24 +0700

    fix epoch looping and add Loss(x) calculation on the fly

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
@@ -14,11 +12,6 @@
 
 #include "nn_assert.h"
 #include "dynamic_array.h"
-
-#define NN_NAME nn 
-#define NN_VERSION_MAJOR 0
-#define NN_VERSION_MINOR 2 
-#define NN_VERSION_PATCH 0
 
 nn_arena arena = {0};
 
@@ -75,7 +68,7 @@ float forward_pass(float x, float y)
     size_t checkpoint = arena.count;
     a = make_mat(1, 2,(float[]) {
             x, y
-        });
+            });
     nn_mat o = make_out(&a, &w);
     nn_mat o2;
 
@@ -119,22 +112,20 @@ void init_params(void)
 int main()
 {
     srand(time(NULL));
-    float mse; // f(x)
-    float mse_2; // f(x + h)
-    float gradient; // (f(x + h) - f(x)) / h
-    // 256 MBs allocation.
-    // Because memory is cheap and not even
-    // Windows is stupid enough to give the 
-    // entire 256 MBs blocks of memory to me lol.
-    // The entire matrix allocation should go through
-    // this point.
+
+    /* 
+     * 256 MBs allocation.
+     * Because memory is cheap and not even Windows is stupid enough to 
+     * give the entire 256 MBs blocks of memory to me lol. The entire 
+     * matrix allocation should go through this point.
+    */ 
     nn_arena_init(&arena, 256 * 1000 * 1000);
 
-    // Dataset for AND Truth Table. The 3rd
-    // index is used to store the result values
-    // of the Neural Network Model. The purpose
-    // of this is to make it easier to calculate
-    // the loss function.
+    /*
+     * index is used to store the result values of the Neural Network 
+     * Model. The purpose of this is to make it easier to calculate
+     * the loss function.
+    */
     nn_mat dataset = make_mat(4, 4, (float[]){
             0, 0, 0, 0,
             1, 0, 0, 0,
@@ -144,94 +135,107 @@ int main()
 
     // For ultra-primitive gradient lookup of the 
     // Loss function to do gradient descent 
-    float epsilon = 0.001;
+    float epsilon = 0.01;
     float learning_rate = 0.05;
-    nn_mat dataset_2 = make_mat(4, 4, (float[]){
-            0, 0, 0, 0,
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            1, 1, 1, 0
-            });
+    float g_w  = 0;
+    float g_w2 = 0;
+    float g_b  = 0;
+    float g_b2 = 0;
+    float mse  = 0;
+
+    float mat_template[] = {
+        0, 0, 0, 0,
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        1, 1, 1, 0
+    };
+    nn_mat dataset_w  = make_mat(4, 4, mat_template);
+    nn_mat dataset_w2 = make_mat(4, 4, mat_template);
+    nn_mat dataset_b  = make_mat(4, 4, mat_template);
+    nn_mat dataset_b2 = make_mat(4, 4, mat_template);
 
     init_params();
     size_t i = 0;
     size_t epoch = 10 * 1000;
+
     for (; i < epoch; ++i) {
         size_t memstate = arena.count;
+
         for (size_t j = 0; j < dataset.rows; ++j) {
             // INITIAL LOSS
             float res = forward_pass(NN_MAT_AT(&dataset, j, 0), NN_MAT_AT(&dataset, j, 1));
             NN_MAT_AT(&dataset, j, 3) = res;
         }
+        mse = loss_mse(&dataset);
+        nn_mat tmp = {0};
 
-        for (size_t j = 0; j < dataset_2.rows; ++j) {
-            size_t state = arena.count;
-            nn_mat init_b = make_mat(b.rows, b.cols, b.es);
+        for (size_t j = 0; j < dataset_b.rows; ++j) {
             nn_mat tmp_b = make_mat(b.rows, b.cols, b.es);
             nn_mat_fill(&tmp_b, epsilon);
             nn_mat_add(&b, &tmp_b, &b);
+            float res = forward_pass(NN_MAT_AT(&dataset_b, j, 0), NN_MAT_AT(&dataset_b, j, 1));
+            nn_mat_sub(&b, &tmp_b, &b);
 
-            nn_mat init_b2 = make_mat(b2.rows, b2.cols, b2.es);
+            NN_MAT_AT(&dataset_b, j, 3) = res;
+        }
+        float mse_b = loss_mse(&dataset_b);
+        float g_b = (mse_b - mse) / epsilon;
+        tmp = make_mat(b.rows, b.cols, b.es);
+        nn_mat_fill(&tmp, (g_b * learning_rate));
+        nn_mat_sub(&b, &tmp, &b);
+
+        for (size_t j = 0; j < dataset_b2.rows; ++j) {
             nn_mat tmp_b2 = make_mat(b2.rows, b2.cols, b2.es);
             nn_mat_fill(&tmp_b2, epsilon);
             nn_mat_add(&b2, &tmp_b2, &b2);
-//
+            float res = forward_pass(NN_MAT_AT(&dataset_b2, j, 0), NN_MAT_AT(&dataset_b2, j, 1));
+            nn_mat_sub(&b2, &tmp_b2, &b2);
+
+            NN_MAT_AT(&dataset_b2, j, 3) = res;
+        }
+        float mse_b2 = loss_mse(&dataset_b2);
+        float g_b2 = (mse_b2 - mse) / epsilon;
+        tmp = make_mat(b2.rows, b2.cols, b2.es);
+        nn_mat_fill(&tmp, (g_b2 * learning_rate));
+        nn_mat_sub(&b2, &tmp, &b2);
+
+        for (size_t j = 0; j < dataset_w.rows; ++j) {
             nn_mat tmp_w = make_mat(w.rows, w.cols, w.es);
             nn_mat_fill(&tmp_w, epsilon);
             nn_mat_add(&w, &tmp_w, &w);
+            float res = forward_pass(NN_MAT_AT(&dataset_w, j, 0), NN_MAT_AT(&dataset_w, j, 1));
+            nn_mat_sub(&w, &tmp_w, &w);
 
+            NN_MAT_AT(&dataset_w, j, 3) = res;
+        }
+        float mse_w = loss_mse(&dataset_w);
+        float g_w = (mse_w - mse) / epsilon;
+        tmp = make_mat(w.rows, w.cols, w.es);
+        nn_mat_fill(&tmp, (g_w * learning_rate));
+        nn_mat_sub(&w, &tmp, &w);
+
+        for (size_t j = 0; j < dataset_w2.rows; ++j) {
             nn_mat tmp_w2 = make_mat(w2.rows, w2.cols, w2.es);
             nn_mat_fill(&tmp_w2, epsilon);
             nn_mat_add(&w2, &tmp_w2, &w2);
-
-            float res = forward_pass(NN_MAT_AT(&dataset_2, j, 0), NN_MAT_AT(&dataset_2, j, 1));
-            NN_MAT_AT(&dataset_2, j, 3) = res;
-
-            nn_mat_sub(&b, &tmp_b, &b);
-            nn_mat_sub(&b2, &tmp_b2, &b2);
-            nn_mat_sub(&w, &tmp_w, &w);
+            float res = forward_pass(NN_MAT_AT(&dataset_w2, j, 0), NN_MAT_AT(&dataset_w2, j, 1));
             nn_mat_sub(&w2, &tmp_w2, &w2);
-            nn_arena_reset_to(&arena, state);
+
+            NN_MAT_AT(&dataset_w2, j, 3) = res;
         }
-
-        mse = loss_mse(&dataset); // f(x)
-        mse_2 = loss_mse(&dataset_2); // f(x + h)
-        gradient = (mse_2 - mse) / epsilon; // (f(x + h) - f(x)) / h
-
-        nn_mat tmp = make_mat(w.rows, w.cols, w.es);
-        nn_mat_fill(&tmp, (gradient * learning_rate));
-        nn_mat_sub(&w, &tmp, &w);
-
+        float mse_w2 = loss_mse(&dataset_w2);
+        float g_w2 = (mse_w2 - mse) / epsilon;
         tmp = make_mat(w2.rows, w2.cols, w2.es);
-        nn_mat_fill(&tmp, (gradient * learning_rate));
+        nn_mat_fill(&tmp, (g_w2 * learning_rate));
         nn_mat_sub(&w2, &tmp, &w2);
 
-        if (i == 0) {
-            printf("- ITERASI - 1 -\n");
-            printf("finished with MSE = %f\n", mse);
-            printf("finished with MSE2 = %f\n", mse_2);
-            printf("finished with gradient = %f\n", gradient);
-            printf("finished with learning = %f\n", gradient * learning_rate);
-            printf("------------------------------\n\n");
-        }
-        tmp = make_mat(b.rows, b.cols, b.es);
-        nn_mat_fill(&tmp, (gradient * learning_rate));
-        nn_mat_sub(&b, &tmp, &b);
-
-        tmp = make_mat(b2.rows, b2.cols, b2.es);
-        nn_mat_fill(&tmp, (gradient * learning_rate));
-        nn_mat_sub(&b2, &tmp, &b2);
+        printf("MSE = %f \n", mse);
         nn_arena_reset_to(&arena, memstate);
     }
 
-    printf("- ITERASI - %zu -\n", i);
+    printf("- ITERASI - 1 -\n");
     printf("finished with MSE = %f\n", mse);
-    printf("finished with MSE2 = %f\n", mse_2);
-    printf("finished with gradient = %f\n", gradient);
-    printf("finished with learning = %f\n", gradient * learning_rate);
-    printf("------------------------------\n");
-    nn_mat_print(&dataset_2);
-    nn_mat_print(&w);
+    nn_mat_print(&dataset);
+    printf("------------------------------\n\n");
     return 0;
 }
-
